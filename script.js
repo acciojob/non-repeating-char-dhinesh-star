cc4451bb24cd2760abc902d34249f19a8a15649c
Dhinesh Kumaran I M <77685230+dhinesh-star@users.noreply.github.com>
2024-06-02 17:34:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let map = new Map();
+	let str = "abcdefab";
+	for(let i=0;i<str.length;i++){
+	    if(map.has(str.charAt(i))==true){
+	        let num = map.get(str.charAt(i));
+	        num++;
+	        map.set(str.charAt(i),num);
+	    }
+	    else{
+	        map.set(str.charAt(i),1);
+	    }
+	}
+	for(let [char,num] of map.entries()){
+	    if(num==1) return char;
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
